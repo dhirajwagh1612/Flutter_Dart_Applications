d7bdb0183b412669d5782e555799847056c2ee1e
dhirajwagh1612 <54485930+dhirajwagh1612@users.noreply.github.com>
2021-05-29 00:10:44 +0530

    updared

--- dicee_rolling_app/lib/main.dart
+++ dicee_rolling_app/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'dart:math';
 void main() {
   return runApp(
     MaterialApp(
@@ -15,39 +15,62 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
-  int leftDiceNumber = 5;
+
+class DicePage extends StatefulWidget {
+  const DicePage({Key key}) : super(key: key);
+
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int RightDiceNumber = 1;
+
+  void changeDiceNumber() {
+    setState(() {
+      RightDiceNumber  = Random().nextInt(6) + 1;
+      leftDiceNumber = Random().nextInt(6) + 1;
+      print('diceNumber = $leftDiceNumber');
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Row(
-      children: <Widget>[
-        Expanded(
+        children: <Widget>[
+          Expanded(
             child: Padding(
               padding: const EdgeInsets.all(16.0),
               child: FlatButton(
-                onPressed: (){
-                  print('Left Button got pressed!');
-                },
+                  onPressed: (){
+                    changeDiceNumber();
+
+                  },
                   child: Image.asset('images/dice$leftDiceNumber.png')
               ),
+            ),
+          ),
+
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child:FlatButton(
+                onPressed: (){
+                  changeDiceNumber();
+
+
+
+                },
+                child: Image.asset('images/dice$RightDiceNumber.png'),
               ),
             ),
-          
-        Expanded(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child:FlatButton(
-            onPressed: (){
-              print('Right button got pressed!');
-            },
-            child: Image.asset('images/dice1.png'),
+
           ),
-        ),
 
-        ),
-
-      ],
+        ],
       ),
     );
   }
